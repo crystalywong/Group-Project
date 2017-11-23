5a09d7f8df60a9ee3f31c51ccf783b7bfe3d2d26
crystalywong <33855731+crystalywong@users.noreply.github.com>
2017-11-23 22:52:38 +0800

    Update gp.cpp

--- gp.cpp
+++ gp.cpp
@@ -16,7 +16,7 @@ int main()
 	//The main loop of the game, only break out when user want to exit
 	while(1){
 		cout << endl;
-		cout << "Welcome Message designed by your group\n" << endl;
+		cout << "Welcome to play the puzzle block game. " << endl;
 		cout << "*** Game Menu ***" << endl;
 		cout << "[1] Start Game" << endl;
 		cout << "[2] Settings" << endl;
